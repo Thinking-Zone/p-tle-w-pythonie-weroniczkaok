0b93069ceef406bacdc7de2be8a7378c24529b73
weroniczkaok <weronika.binkowskaa@gmail.com>
2024-11-07 09:55:21 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,3 @@
+#pętla for jest używana, gdy znamy góry liczbę iteracji, ktore chcemy wykonac
+# a petla while jest uzywana gdy nie wiemy ile iteracji potrzebijemy i gdy chcemy by petla
+#trwala dlugo i warunek musi byc spelniony
